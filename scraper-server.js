0ed46e27cad639e3ab3d2e6b2169864651c958d5
Shannor Trotty <shannor.a.trotty@gmail.com>
2017-01-09 23:42:12 -0500

    Changed methods to reflect the change in website

--- scraper-server.js
+++ scraper-server.js
@@ -7,10 +7,12 @@ const async   = require('async');
 
 const baseURL = "http://www.readcomics.tv/";
 
+
+//TODO: Need an newly updated get request for homepage
 //Get request to get all comics on the website in Alpha/ Numberic order
-app.get('/scrape-all', function(req, res){
-  //URL to hit for all the comics 
-  var url = "http://comicastle.org/manga-list.html?listType=allABC";
+app.get('/comic-list-AZ', function(req, res){
+  //URL to hit for all the comics A-Z
+  var url = baseURL + "comic-list";
   
   request(url, function(error, response, html){
         // First we'll check to make sure no errors occurred when making the request
@@ -21,22 +23,18 @@ app.get('/scrape-all', function(req, res){
             // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
             var $ = cheerio.load(html);
 
-            $('span').each(function(i, elm){
-                //Only get manga enteries  
-                if( $(this).attr('data-toggle') == 'mangapop'){
+            $('.container').find('li').each(function(){
                   //Only has the title and link present here 
                   var json = { title: "", link: ""};
-                  
-                  json.title = $(this).attr('data-original-title').toString();
-                  json.link = $(this).children().children().attr('href').toString();
-                  var category = $(this).siblings('h3').text();
+                  json.title = $(this).children().text();
+                  json.link = $(this).children().attr('href').toString();
+                  var category = $(this).parent().siblings('div').text();
                   //Add comic to correct category
-                  if(category == "#"){
+                  if(category == "#" || !isNaN(category)){
                       comics.Number.push(json);
                   }else{
                       comics[category].push(json);
                   }
-                }
             });
         }
         //Return the Array of all the JSON comic objects
@@ -45,11 +43,12 @@ app.get('/scrape-all', function(req, res){
     });
 });
 
-//GET method to receive comics by popluarity or other features
-app.get('/scrape-pages', function(req, res){
+//TODO: Could ask for page number?
+//GET method to receive list of popular comics
+app.get('/popular-comics/:pageNumber', function(req, res){
     //Url to hit
-    var url = "http://comicastle.org/manga-list.html?listType=pagination&page=1&artist=&author=&name=&genre=&sort=views&sort_type=DESC";
-  request(url, function(error, response, html){
+    var url = baseURL + 'popular-comic/' + req.params.pageNumber;
+    request(url, function(error, response, html){
         // First we'll check to make sure no errors occurred when making the request
         if(!error){
             // Var to hold all the comic information 
@@ -57,35 +56,33 @@ app.get('/scrape-pages', function(req, res){
             // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
             var $ = cheerio.load(html);
 
-            $('span').each(function(){
-                //Only get manga entries 
-                if( $(this).attr('data-toggle') == 'mangapop'){
-                  //This page has more fields to pick from. 
-                  var json = { 
-                      title: "", 
-                      link: "",
-                      img: "", 
-                      genre: [] , 
-                      totalViews: 0, 
-                      lastIssueLink: "", 
-                      lastIssueNum:0 
+            $('.manga-box').each(function(){
+                //This page has more fields to pick from. 
+                var json = { 
+                    title: "", 
+                    link: "",
+                    img: "", 
+                    genre: []
+                };
+
+                json.title = $(this).find('h3').children().text();
+                json.link = $(this).find('h3').children().attr('href');
+                json.img = $(this).find('img').attr('src').toString();
+                //Loop through all possible genres
+
+                $(this).find('.tags').each(function(){
+                    var genreJson = {
+                        name: "",
+                        genreLink: ""
                     };
+                    genreJson.name = $(this).text();
+                    genreJson.genreLink = $(this).attr('href');
+                    json.genre.push(genreJson);
+                });
 
-                  json.title = $(this).attr('data-original-title').toString();
-                  json.link = $(this).children('.media').children().attr('href').toString();
-                  json.img = $(this).find('img').attr('src').toString();
-                  //Loop through all possible genres
-                  $(this).find('small').each(function(){
-                    json.genre.push($(this).text());
-                  });
-
-                  json.totalViews = $(this).children('.media').find('span').text();
-                  json.lastIssueLink = $(this).find('small').siblings('a').attr('href');
-                  json.lastIssueNum = $(this).find('small').siblings('a').text();
-                  //Add to comics array
-                  comics.push(json);
-                }
-            });
+                //Add to comics array
+                comics.push(json);
+        });
 
         }
         //Return the Created JSON Dictionary. 
@@ -97,35 +94,34 @@ app.get('/scrape-pages', function(req, res){
 });
 
 //GET method to return the list of all Issue of a Comic 
-app.get('/listIssues/:comicUrl', function(req, res){
+app.get('/listIssues/:comicName', function(req, res){
 
-    var url = baseURL + req.params.comicUrl;
+    var url = baseURL +'comic/' + req.params.comicName;
 
     request(url, function(error, response, html){
         // First we'll check to make sure no errors occurred when making the request
         if(!error){
-            var listOfComics = [];
+            var listOfIssues = [];
 
             var $ = cheerio.load(html);
 
-            $('tr').each(function(){
+            $('.basic-list').children().each(function(){
 
                 var json = {
                     chapterName: "", 
-                    link: ""
+                    link: "",
+                    releaseDate: ""
                 };
 
-                var link = $(this).find('a').attr('href');
+                json.link = $(this).children('a').attr('href');
                 //Null check and make sure its a link to reading 
-                if( link != null && link.indexOf("read") >= 0){
-                    json.link = link;
-                    json.chapterName = $(this).find('a').text();
-                    listOfComics.push(json);
-                }
+                json.chapterName = $(this).children('a').text();
+                json.releaseDate = $(this).children('span').text();
+                listOfIssues.push(json);
             });
         }
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(listOfComics,null, 3));
+        res.send(JSON.stringify(listOfIssues,null, 3));
     });
 });
 
